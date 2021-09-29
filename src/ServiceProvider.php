5b41f20d71cd4aba1e9a9018a06239a10c9aae45
Jack Sleight <jacksleight@gmail.com>
2021-09-29 17:44:24 +0100

    Service provider should call parents

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -5,11 +5,15 @@ namespace JackSleight\BardMutator;
 use Statamic\Providers\AddonServiceProvider;
 use Statamic\Fieldtypes\Bard\Augmentor;
 use JackSleight\BardMutator\Mutator;
+use JackSleight\BardMutator\Nodes;
+use JackSleight\BardMutator\Marks;
 
 class ServiceProvider extends AddonServiceProvider
 {
     public function register()
     {
+        parent::register();
+        
         $this->app->singleton(Mutator::class, function () {
             return new Mutator();
         });
@@ -17,32 +21,34 @@ class ServiceProvider extends AddonServiceProvider
     
     public function boot()
     {
+        parent::boot();
+
         $classes = [
             'replaceNodes' => [
-                \ProseMirrorToHtml\Nodes\Blockquote::class       => \JackSleight\BardMutator\Nodes\Blockquote::class,
-                \ProseMirrorToHtml\Nodes\BulletList::class       => \JackSleight\BardMutator\Nodes\BulletList::class,
-                \ProseMirrorToHtml\Nodes\CodeBlock::class        => \JackSleight\BardMutator\Nodes\CodeBlock::class,
-                \ProseMirrorToHtml\Nodes\HardBreak::class        => \JackSleight\BardMutator\Nodes\HardBreak::class,
-                \ProseMirrorToHtml\Nodes\Heading::class          => \JackSleight\BardMutator\Nodes\Heading::class,
-                \ProseMirrorToHtml\Nodes\HorizontalRule::class   => \JackSleight\BardMutator\Nodes\HorizontalRule::class,
-                \Statamic\Fieldtypes\Bard\ImageNode::class       => \JackSleight\BardMutator\Nodes\Image::class,
-                \ProseMirrorToHtml\Nodes\ListItem::class         => \JackSleight\BardMutator\Nodes\ListItem::class,
-                \ProseMirrorToHtml\Nodes\OrderedList::class      => \JackSleight\BardMutator\Nodes\OrderedList::class,
-                \ProseMirrorToHtml\Nodes\Paragraph::class        => \JackSleight\BardMutator\Nodes\Paragraph::class,
-                \ProseMirrorToHtml\Nodes\Table::class            => \JackSleight\BardMutator\Nodes\Table::class,
-                \ProseMirrorToHtml\Nodes\TableCell::class        => \JackSleight\BardMutator\Nodes\TableCell::class,
-                \ProseMirrorToHtml\Nodes\TableHeader::class      => \JackSleight\BardMutator\Nodes\TableHeader::class,
-                \ProseMirrorToHtml\Nodes\TableRow::class         => \JackSleight\BardMutator\Nodes\TableRow::class,
+                \ProseMirrorToHtml\Nodes\Blockquote::class       => Nodes\Blockquote::class,
+                \ProseMirrorToHtml\Nodes\BulletList::class       => Nodes\BulletList::class,
+                \ProseMirrorToHtml\Nodes\CodeBlock::class        => Nodes\CodeBlock::class,
+                \ProseMirrorToHtml\Nodes\HardBreak::class        => Nodes\HardBreak::class,
+                \ProseMirrorToHtml\Nodes\Heading::class          => Nodes\Heading::class,
+                \ProseMirrorToHtml\Nodes\HorizontalRule::class   => Nodes\HorizontalRule::class,
+                \Statamic\Fieldtypes\Bard\ImageNode::class       => Nodes\Image::class,
+                \ProseMirrorToHtml\Nodes\ListItem::class         => Nodes\ListItem::class,
+                \ProseMirrorToHtml\Nodes\OrderedList::class      => Nodes\OrderedList::class,
+                \ProseMirrorToHtml\Nodes\Paragraph::class        => Nodes\Paragraph::class,
+                \ProseMirrorToHtml\Nodes\Table::class            => Nodes\Table::class,
+                \ProseMirrorToHtml\Nodes\TableCell::class        => Nodes\TableCell::class,
+                \ProseMirrorToHtml\Nodes\TableHeader::class      => Nodes\TableHeader::class,
+                \ProseMirrorToHtml\Nodes\TableRow::class         => Nodes\TableRow::class,
             ],
             'replaceMarks' => [
-                \ProseMirrorToHtml\Marks\Bold::class             => \JackSleight\BardMutator\Marks\Bold::class,
-                \ProseMirrorToHtml\Marks\Code::class             => \JackSleight\BardMutator\Marks\Code::class,
-                \ProseMirrorToHtml\Marks\Italic::class           => \JackSleight\BardMutator\Marks\Italic::class,
-                \Statamic\Fieldtypes\Bard\LinkMark::class        => \JackSleight\BardMutator\Marks\Link::class,
-                \ProseMirrorToHtml\Marks\Subscript::class        => \JackSleight\BardMutator\Marks\Subscript::class,
-                \ProseMirrorToHtml\Marks\Underline::class        => \JackSleight\BardMutator\Marks\Underline::class,
-                \ProseMirrorToHtml\Marks\Strike::class           => \JackSleight\BardMutator\Marks\Strike::class,
-                \ProseMirrorToHtml\Marks\Superscript::class      => \JackSleight\BardMutator\Marks\Superscript::class,
+                \ProseMirrorToHtml\Marks\Bold::class             => Marks\Bold::class,
+                \ProseMirrorToHtml\Marks\Code::class             => Marks\Code::class,
+                \ProseMirrorToHtml\Marks\Italic::class           => Marks\Italic::class,
+                \Statamic\Fieldtypes\Bard\LinkMark::class        => Marks\Link::class,
+                \ProseMirrorToHtml\Marks\Subscript::class        => Marks\Subscript::class,
+                \ProseMirrorToHtml\Marks\Underline::class        => Marks\Underline::class,
+                \ProseMirrorToHtml\Marks\Strike::class           => Marks\Strike::class,
+                \ProseMirrorToHtml\Marks\Superscript::class      => Marks\Superscript::class,
             ],
         ];
         foreach ($classes['replaceNodes'] as $searchNode => $mutatorNode) {
