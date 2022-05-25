317eac1145e2ad1f5c231115fffdde9e216f4e96
Jack Sleight <jacksleight@gmail.com>
2022-05-25 20:43:53 +0100

    WIP

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -6,10 +6,6 @@ use Statamic\Providers\AddonServiceProvider;
 
 class ServiceProvider extends AddonServiceProvider
 {
-    protected $scripts = [
-        __DIR__.'/../dist/js/addon.js',
-    ];
-
     protected $tags = [
         Tags\MutatorTag::class,
     ];
